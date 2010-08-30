9c061d0cdcc4e1971524bfe8eba5827ad2f11b2c
Scott Kirkland <srkirkland@gmail.com>
2010-08-30 11:37:00 -0700

    added global settings and web.config settings

---
+++ UCDMvcBootCamp/ComponentRegistrar.asax.cs
@@ -0,0 +1,32 @@
+﻿using Castle.Windsor;
+using UCDArch.Core.CommonValidator;
+using UCDArch.Core.NHibernateValidator.CommonValidatorAdapter;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Data.NHibernate;
+
+namespace UCDMvcBootCamp
+{
+    public static class ComponentRegistrar
+    {
+        public static void AddComponentsTo(IWindsorContainer container)
+        {
+            AddGenericRepositoriesTo(container);
+
+            container.AddComponent("validator",
+                                   typeof(IValidator), typeof(Validator));
+            container.AddComponent("dbContext", typeof(IDbContext), typeof(DbContext));
+
+        }
+
+        private static void AddGenericRepositoriesTo(IWindsorContainer container)
+        {
+            container.AddComponent("repositoryWithTypedId",
+                                   typeof(IRepositoryWithTypedId<,>), typeof(RepositoryWithTypedId<,>));
+            container.AddComponent("repositoryType",
+                                   typeof(IRepository<>), typeof(Repository<>));
+            container.AddComponent("repository",
+                                   typeof(IRepository), typeof(Repository));
+
+        }
+    }
+}
--- UCDMvcBootCamp/Global.asax.cs
+++ UCDMvcBootCamp/Global.asax.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
+using Castle.Windsor;
+using Microsoft.Practices.ServiceLocation;
+using MvcContrib.Castle;
+using UCDArch.Web.IoC;
+using UCDArch.Web.ModelBinder;
+using UCDArch.Web.Validator;
+using UCDMvcBootCamp.Controllers;
 
 namespace UCDMvcBootCamp
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
-
     public class MvcApplication : System.Web.HttpApplication
     {
         public static void RegisterRoutes(RouteCollection routes)
@@ -17,18 +17,41 @@ namespace UCDMvcBootCamp
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                "Default",                                              // Route name
+                "{controller}/{action}/{id}",                           // URL with parameters
+                new { controller = "Home", action = "Index", id = "" }  // Parameter defaults
             );
 
         }
 
+// ReSharper disable InconsistentNaming
         protected void Application_Start()
+// ReSharper restore InconsistentNaming
         {
-            AreaRegistration.RegisterAllAreas();
+            #if DEBUG
+            HibernatingRhinos.Profiler.Appender.NHibernate.NHibernateProfiler.Initialize();
+            #endif
+
+            xVal.ActiveRuleProviders.Providers.Add(new ValidatorRulesProvider());
 
             RegisterRoutes(RouteTable.Routes);
+
+            ModelBinders.Binders.DefaultBinder = new UCDArchModelBinder();
+
+            IWindsorContainer container = InitializeServiceLocator();
+        }
+
+        private static IWindsorContainer InitializeServiceLocator()
+        {
+            IWindsorContainer container = new WindsorContainer();
+            ControllerBuilder.Current.SetControllerFactory(new WindsorControllerFactory(container));
+
+            container.RegisterControllers(typeof(HomeController).Assembly);
+            ComponentRegistrar.AddComponentsTo(container);
+
+            ServiceLocator.SetLocatorProvider(() => new WindsorServiceLocator(container));
+
+            return container;
         }
     }
 }
